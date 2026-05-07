bea717e70d14cf727ca3349ec0fa1113e9e7ffc4
OKO3210 <124586074+OKO3210@users.noreply.github.com>
2026-05-07 11:37:43 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from data_extractor_controller import DataExtractorController
-
-
-app = QApplication([])
-controller = DataExtractorController()
-app.exec_()
